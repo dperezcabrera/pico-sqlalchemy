8e0b83cf0c18620e78f2672ae8de5d895c2aa6d8
T151232 <dperezcabrera@gmail.com>
2025-11-18 23:32:59 +0100

    refactor: remove global SessionManager state and use AOP for @transactional

--- src/pico_sqlalchemy/decorators.py
+++ src/pico_sqlalchemy/decorators.py
@@ -1,16 +1,11 @@
-from typing import Any, Callable, Optional, ParamSpec, TypeVar, Set
-from pico_ioc import component
-import inspect
-
-from .session import get_default_session_manager
+from typing import Any, Callable, Optional, ParamSpec, TypeVar
+from pico_ioc import component, intercepted_by
 
 P = ParamSpec("P")
 R = TypeVar("R")
 
 TRANSACTIONAL_META = "_pico_sqlalchemy_transactional_meta"
 REPOSITORY_META = "_pico_sqlalchemy_repository_meta"
-REPOSITORIES: Set[type] = set()
-
 
 def transactional(
     *,
@@ -41,36 +36,10 @@ def transactional(
 
     def decorator(func: Callable[P, R]) -> Callable[P, R]:
         setattr(func, TRANSACTIONAL_META, metadata)
-
-        if inspect.iscoroutinefunction(func):
-
-            async def async_wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-                manager = get_default_session_manager()
-                if manager is None:
-                    return await func(*args, **kwargs)
-                async with manager.transaction(
-                    propagation=propagation,
-                    read_only=read_only,
-                    isolation_level=isolation_level,
-                    rollback_for=rollback_for,
-                    no_rollback_for=no_rollback_for,
-                ):
-                    return await func(*args, **kwargs)
-
-            setattr(async_wrapper, TRANSACTIONAL_META, metadata)
-            return async_wrapper
-
-        def sync_wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-            raise TypeError(
-                f"Cannot apply @transactional to sync function '{func.__name__}' "
-                "when using an async SessionManager."
-            )
-
-        setattr(sync_wrapper, TRANSACTIONAL_META, metadata)
-        return sync_wrapper
+        from .interceptor import TransactionalInterceptor
+        return intercepted_by(TransactionalInterceptor)(func)
 
     return decorator
-
 
 def repository(
     cls: Optional[type[Any]] = None,
@@ -80,7 +49,6 @@ def repository(
 ) -> Callable[[type[Any]], type[Any]] | type[Any]:
     def decorate(c: type[Any]) -> type[Any]:
         setattr(c, REPOSITORY_META, kwargs)
-        REPOSITORIES.add(c)
         return component(c, scope=scope, **kwargs)
 
     if cls is not None:
